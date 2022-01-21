690453fb40486daa3f0eec1cefef97fcd5cb113c
DraganT <dragan_tom@yahoo.com>
2022-01-21 15:01:02 +0100

    Added New Class Television

---
+++ src/main/java/main/multimedia_dragan_tomasevic/Television.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.multimedia_dragan_tomasevic;
+
+/**
+ *
+ * @author Dragan
+ */
+public class Television {
+    private int volume=0;
+    private int currentProgram=1;
+    private boolean turnOn=false;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getCurrentProgram() {
+        return currentProgram;
+    }
+
+    public void setCurrentProgram(int currentProgram) {
+        this.currentProgram = currentProgram;
+    }
+
+    public boolean isTurnOn() {
+        return turnOn;
+    }
+
+    public void setTurnOn(boolean turnOn) {
+        this.turnOn = turnOn;
+    }
+    
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
