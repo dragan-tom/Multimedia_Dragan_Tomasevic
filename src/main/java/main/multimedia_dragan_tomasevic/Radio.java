d95611083af2e5ba41d0e4875429725fed46d9dc
DraganT <dragan_tom@yahoo.com>
2022-01-21 15:07:44 +0100

    New Class Radio

---
+++ src/main/java/main/multimedia_dragan_tomasevic/Radio.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.multimedia_dragan_tomasevic;
+
+/**
+ *
+ * @author Dragan
+ */
+public class Radio {
+    
+    private double fmFrequency;
+    private int amFrequency;
+    private String band;
+
+    public double getFmFrequency() {
+        return fmFrequency;
+    }
+
+    public void setFmFrequency(double fmFrequency) {
+        this.fmFrequency = fmFrequency;
+    }
+
+    public int getAmFrequency() {
+        return amFrequency;
+    }
+
+    public void setAmFrequency(int amFrequency) {
+        this.amFrequency = amFrequency;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+    
+            
+    
+    
+    
+    
+    
+}
